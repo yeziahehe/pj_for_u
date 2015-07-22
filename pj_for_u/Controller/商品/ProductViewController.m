91da4af832e4f40401612e651452e155639c6e0f
ivan.ye <yeziahehe@gmail.com>
2015-07-22 15:11:20 +0800

    userinfo nav

---
+++ pj_for_u/Controller/商品/ProductViewController.m
@@ -0,0 +1,32 @@
+//
+//  ProductViewController.m
+//  pj_for_u
+//
+//  Created by 叶帆 on 15/7/22.
+//  Copyright (c) 2015年 叶帆. All rights reserved.
+//
+
+#import "ProductViewController.h"
+
+@interface ProductViewController ()
+
+@end
+
+@implementation ProductViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
