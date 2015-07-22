cb0b59cbc8d57e5673081e1b175ca1e16c5fccf5
miao <870291020@qq.com>
2015-07-22 13:57:50 +0800

    首页头图开始

--- pj_for_u/Controller/首页/ImageContainView.h
+++ pj_for_u/Controller/首页/ImageContainView.h
@@ -8,6 +8,19 @@
 
 #import "HomeSubView.h"
 
-@interface ImageContainView : HomeSubView
+@interface ImageContainView : HomeSubView<YFCycleScrollViewDelegate>
 
+@protocol ImagesContainViewDelegate;
+
+@property (strong, nonatomic) IBOutlet YFCycleScrollView *cycleScrollView;
+@property (strong, nonatomic) IBOutlet UIPageControl *pageControl;
+@property (nonatomic, assign) id<ImagesContainViewDelegate> delegate;
+@property (strong, nonatomic) NSArray *productAdArray;
+
+- (void)reloadWithProductAds:(NSArray *)productAds;
+@end
+
+@protocol ImagesContainViewDelegate <NSObject>
+
+- (void)didTappedWithProductAd:(AdModel *)productAd;
 @end
