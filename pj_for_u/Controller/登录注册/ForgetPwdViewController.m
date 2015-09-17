61c2fa9d7f18b1dfbf3060e48d365bb7aa2d5c3e
niuyan <632176758@qq.com>
2015-09-17 14:31:32 +0800

    pull

--- pj_for_u/Controller/登录注册/ForgetPwdViewController.m
+++ pj_for_u/Controller/登录注册/ForgetPwdViewController.m
@@ -90,45 +90,41 @@
 #pragma mark - IBAction Methods
 - (IBAction)identifyButtonClicked:(id)sender {
     [self resignAllField];
-    
-    //判断号码是否注册，如未注册，则显示该号码未注册
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
-    //接口地址
-    NSString *url = [NSString stringWithFormat:@"%@%@",kServerAddress,kCheckUserExistUrl];
-    //传递参数存放的字典
-    
-    NSString *phoneNum = self.phoneTextField.text;
-    NSLog(@"shit %@",phoneNum);
-    NSMutableDictionary *dict = kCommonParamsDict;
-    [dict setObject:phoneNum forKey:@"phone"];
-    
-    //进行post请求
-    [manager POST:url
-       parameters:dict
-          success:^(AFHTTPRequestOperation *operation,id responseObject) {
-              
-              NSString *validString = [self checkFieldValid];
-              if(validString)
-              {
-                  [[YFProgressHUD sharedProgressHUD] showWithMessage:validString customView:nil hideDelay:2.f];
+    NSString *validString = [self checkFieldValid];
+    if(validString)
+    {
+        [[YFProgressHUD sharedProgressHUD] showWithMessage:validString customView:nil hideDelay:2.f];
+    }else{
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+        //接口地址
+        NSString *url = [NSString stringWithFormat:@"%@%@",kServerAddress,kCheckUserExistUrl];
+        //传递参数存放的字典
+        NSString *phone = self.phoneTextField.text;
+        NSMutableDictionary *dict = kCommonParamsDict;
+        [dict setObject:phone forKey:@"phone"];
+        //进行post请求
+        [manager POST:url
+           parameters:dict
+              success:^(AFHTTPRequestOperation *operation,id responseObject) {
+                  
+                  NSString *status = [responseObject objectForKey:@"status"];
+                  if ([status isEqualToString:@"failure"]) {
+                        self.identifyButton.enabled = NO;
+                        self.resendSecond = kResendTimeCount;
+                        self.resendTimer = [NSTimer scheduledTimerWithTimeInterval:1.f target:self selector:@selector(resendTimerChange) userInfo:nil repeats:YES];
+                        [[YFProgressHUD sharedProgressHUD]showWithMessage:@"验证码发送中，请稍等……" customView:nil hideDelay:2.f];
+                        [self getVerifyCode];
+                  }else{
+                      [[YFProgressHUD sharedProgressHUD] showWithMessage:@"该账号尚未注册，请注册" customView:nil hideDelay:2.f];
+                  }
               }
-              else
-              {
-                  self.identifyButton.enabled = NO;
-                  self.resendSecond = kResendTimeCount;
-                  self.resendTimer = [NSTimer scheduledTimerWithTimeInterval:1.f target:self selector:@selector(resendTimerChange) userInfo:nil repeats:YES];
-                  [[YFProgressHUD sharedProgressHUD]showWithMessage:@"验证码发送中，请稍等……" customView:nil hideDelay:2.f];
-                  [self getVerifyCode];
-              }
-
-          }
-
-          failure:^(AFHTTPRequestOperation *operation,NSError *error) {
-              [[YFProgressHUD sharedProgressHUD] showFailureViewWithMessage:@"该号码未被注册，请先注册账号" hideDelay:2.f];
-              NSLog(@"Error: %@", error);
-              
-          }];
+              failure:^(AFHTTPRequestOperation *operation,NSError *error) {
+                  
+                  NSLog(@"Error: %@", error);
+                  
+              }];
+    }
 }
 
 - (IBAction)resetPwdButtonClicked:(id)sender {
